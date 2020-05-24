ceaf23dfd75db942d8594d6a7c639a23822df611
Yogesh Yogendra <53307155+Sn0wGod@users.noreply.github.com>
2020-05-24 21:23:37 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -41,6 +41,31 @@ self.addEventListener('install',event=>{
   )
 })
 
+self.addEventListener('activate',function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+
+        }).map(function(cacheName){
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response){
+      if(response){
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
 
 
 
